0bfdf3c20d69f2ff27488ff69a5498241596038b
RZFury <48477044+rzfury@users.noreply.github.com>
2020-10-07 06:15:01 +0700

    fix asset prefix

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
 
 module.exports = {
-  assetPrefix: process.env.NODE_ENV === 'production' ? './rz-nextjs-tailwind-typescript' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? './' : '',
 }
