8e403ea2af3929b88f088f98d02a201ecb4b1573
RZFury <48477044+rzfury@users.noreply.github.com>
2020-10-09 14:17:01 +0700

    tidying card.tsx

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -1,24 +1,12 @@
 import React from 'react';
 import conclass from '../../utility/conclass';
 
-class Card extends React.Component<RazorWindProps.Card.Card, any> {
-  render() {
-    return (
-      <div
-        className={
-          conclass(
-            'block rounded',
-            this.props.shadow
-            ? 'shadow-md'
-            : 'border-solid border border-gray-400',
-            this.props.asBody ? 'p-4' : ''
-          )
-        }
-      >
-        {this.props.children}
-      </div>
-    );
-  }
+function Card(props: RazorWindProps.Card.Card) {
+  return (
+    <div className={conclass('block rounded', this.props.shadow ? 'shadow-md' : 'border-solid border border-gray-400', this.props.asBody ? 'p-4' : '')}>
+      {this.props.children}
+    </div>
+  );
 }
 
 export default Card;
