db0cd89dbacf7fa723b8a975c1bd28c6ffd441f1
RZFury <48477044+rzfury@users.noreply.github.com>
2020-11-02 17:26:35 +0700

    tidying control imports

---
+++ src/components/form/index.ts
@@ -0,0 +1,19 @@
+import AutoComplete from './autocomplete';
+import Checkbox from './checkbox';
+import Field from './field';
+import FieldArea from './field-area';
+import Form from './form';
+import FormGroup from './group';
+import Radio from '../../components/form/radio';
+import Select from './select';
+
+export {
+  AutoComplete,
+  Checkbox,
+  Field,
+  FieldArea,
+  Form,
+  FormGroup,
+  Radio,
+  Select
+};
