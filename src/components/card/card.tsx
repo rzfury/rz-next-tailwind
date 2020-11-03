cb776e7642f02b30eb2b734a7365363dd72787fb
RZFury <48477044+rzfury@users.noreply.github.com>
2020-11-03 09:40:10 +0700

    add header and footer to card

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -1,12 +1,30 @@
 import React from 'react';
 import conclass from '../../utility/conclass';
 
-function Card(props: RazorWindProps.Card.Card) {
-  return (
-    <div className={conclass('block rounded', props.shadow ? 'shadow-md' : 'border-solid border border-gray-400', props.asBody ? 'p-4' : '')}>
-      {props.children}
-    </div>
-  );
+class Card extends React.Component<RazorWindProps.Card.Card, {}> {
+  renderSimpleTitle = (content: any, isFooter?: boolean) => {
+    if (content) {
+      return (
+        <div className={conclass('bg-gray-200 p-4', isFooter ? 'rounded-b' : 'rounded-t', this.props.shadow ? '' : 'border-b border-gray-300')}>{content}</div>
+      );
+    }
+    else {
+      return <></>;
+    }
+  }
+
+  render() {
+    return (
+      <div className={conclass('block rounded', this.props.shadow ? 'shadow-md' : 'border-solid border border-gray-300')}>
+        {this.renderSimpleTitle(this.props.title)}
+        {this.props.asBody
+          ? <div className="p-4">{this.props.children}</div>
+          : this.props.children
+        }
+        {this.renderSimpleTitle(this.props.footer, true)}
+      </div>
+    );
+  }
 }
 
 export default Card;
--- src/pages/examples/card.tsx
+++ src/pages/examples/card.tsx
@@ -1,14 +1,33 @@
 import React from 'react';
 import Card from '../../components/card/card';
-import CardBody from '../../components/card/body';
 
 export default function ExampleCard() {
   return (
     <div className="p-5">
       <span className="text-gray-700 text-2xl">TailwindCSS - Card</span>
       <hr className="my-5" />
-      <Card shadow asBody>
+      <Card>
+        Card plain
+      </Card>
+      <br/>
+      <Card asBody>
         Card Body
+      </Card>
+      <br/>
+      <Card shadow asBody>
+        Card Body + Shadow
+      </Card>
+      <br/>
+      <Card asBody title="Simple title">
+        Card Body + Simple Title
+      </Card>
+      <br/>
+      <Card shadow asBody title="Simple title">
+        Card Body + Shadow + Simple Title
+      </Card>
+      <br/>
+      <Card shadow asBody title="Simple title" footer="Simple Footer">
+        Card Body + Shadow + Simple Title + Simple Footer
       </Card>
     </div>
   );
