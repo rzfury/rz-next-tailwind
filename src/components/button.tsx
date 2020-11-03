23f06cc3bd03917a9990faf2d121d150d008a672
RZFury <48477044+rzfury@users.noreply.github.com>
2020-11-03 09:38:25 +0700

    tidying button, i think

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -2,5 +2,6 @@ import React from 'react';
 import conclass from '../utility/conclass';
 
 export default function Button(props: RazorWindProps.Button) {
-  return <button {...props} className={conclass('btn', 'btn-'+props.role, props.block && 'btn-block', props.rounded && 'rounded', props.outline && 'btn-outline', props.className)}>{props.children}</button>;
+  const c = (a: boolean, b: string) => a ? b : '';
+  return <button {...props} className={conclass('btn', 'btn-'+props.role, c(props.block, 'btn-block'), c(props.rounded, 'rounded'), c(props.outline, 'btn-outline'), props.className)}>{props.children}</button>;
 }
