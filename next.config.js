815c15cf2adaee168e9e85964c9c4844f5743d75
RZFury <48477044+rzfury@users.noreply.github.com>
2020-12-08 10:14:51 +0700

    remove travis

--- next.config.js
+++ next.config.js
@@ -1,5 +1,3 @@
 require('dotenv').config();
 
-module.exports = {
-  assetPrefix: process.env.NODE_ENV === 'production' ? 'https://raw.githack.com/rzfury/rz-next-tailwind/gh-pages/' : '',
-}
+module.exports = {}
