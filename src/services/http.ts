f5a8e9dc976a3376f09cfae27a37393e6eef8089
RZFury <48477044+rzfury@users.noreply.github.com>
2020-12-11 14:10:06 +0700

    add parse params

--- src/services/http.ts
+++ src/services/http.ts
@@ -1,8 +1,11 @@
 import fetch from 'node-fetch';
 
-export async function get(url: string) {
+export async function get(url: string, params?: { [key:string]: any }) {
+  const _params = new URLSearchParams(params);
+  const _url = params ? `${url}?${_params.toString()}` : url;
+
   return new Promise((resolve, reject) => {
-    fetch(url)
+    fetch(_url)
       .then(async (res: any) => {
         const data = await res.json();
         resolve(data);
@@ -14,9 +17,12 @@ export async function get(url: string) {
   });
 }
 
-export async function post(url: string, body:Object) {
+export async function post(url: string, body: Object, params?: { [key:string]: any }) {
+  const _params = new URLSearchParams(params);
+  const _url = params ? `${url}?${_params.toString()}` : url;
+
   return new Promise((resolve, reject) => {
-    fetch(url, { method: 'post', body: JSON.stringify(body) })
+    fetch(_url, { method: 'post', body: JSON.stringify(body) })
       .then(async (res: any) => {
         const data = await res.json();
         resolve(data);
